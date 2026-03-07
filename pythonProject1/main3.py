6d5e3ee28c5fb597e294f27e6778f571dd16db3b
EscRostik <agllore09@gmail.com>
2026-03-07 17:42:29 +0200

    67!!!!!!!!!!

---
+++ pythonProject1/main3.py
@@ -0,0 +1,18 @@
+import turtle
+t = turtle.Turtle()
+t.color("red")
+t.backward(100)
+t.right(90)
+t.forward(75)
+for i in range(4):
+     t.forward(100)
+     t.left(90)
+t.penup()
+t.backward(75)
+t.left(90)
+t.forward(150)
+t.pendown()
+t.forward(100)
+t.right(120)
+t.forward(205)
+turtle.done()
